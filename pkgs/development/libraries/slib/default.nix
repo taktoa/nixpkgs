139d773aceb5fb887a8208add4dc8a1a0ea8d073
Remy Goldschmidt <taktoa@gmail.com>
2015-10-05 15:44:56 -0500

    slib: 3b2 -> 3b5

--- pkgs/development/libraries/slib/default.nix
+++ pkgs/development/libraries/slib/default.nix
@@ -1,33 +1,35 @@
-{ fetchurl, stdenv, unzip, scheme, texinfo }:
+{ fetchurl, stdenv, unzip, findutils, texinfo  # Build dependencies
+, scheme                                       # Scheme compiler
+}:
 
 stdenv.mkDerivation rec {
-  name = "slib-3b2";
+  name = "slib-3.5";
 
   src = fetchurl {
-    url = "http://groups.csail.mit.edu/mac/ftpdir/scm/${name}.zip";
-    sha256 = "1s6a7f3ha2bhwj4nkg34n0j511ww1nlgrn5xis8k53l8ghdrrjxi";
+    url = "http://groups.csail.mit.edu/mac/ftpdir/scm/slib-3b5.zip";
+    sha256 = "0q0p2d53p8qw2592yknzgy2y1p5a9k7ppjx0cfrbvk6242c4mdpq";
   };
 
   patches = [ ./catalog-in-library-vicinity.patch ];
 
-  buildInputs = [ unzip scheme texinfo ];
+  buildInputs = [ unzip findutils scheme texinfo ];
 
-  configurePhase = ''
-    mkdir -p "$out"
-    sed -i "Makefile" \
-        -e "s|^[[:blank:]]*prefix[[:blank:]]*=.*$|prefix = $out/|g"
+  preConfigure = ''
+      find . -type f -exec chmod -x {} \;
+      chmod +x configure
+      mkdir -p "$out"
   '';
 
   buildPhase = "make infoz";
 
   installPhase = ''
-    make install
-
-    ln -s mklibcat{.scm,}
-    SCHEME_LIBRARY_PATH="$out/lib/slib" make catalogs
-
-    sed -i "$out/bin/slib" \
-        -e "/^SCHEME_LIBRARY_PATH/i export PATH=\"${scheme}/bin:\$PATH\""
+      make install
+   
+      ln -s mklibcat.scm mklibcat
+      SCHEME_LIBRARY_PATH="$out/lib/slib" make catalogs
+   
+      sed -i "$out/bin/slib" \
+          -e "/^SCHEME_LIBRARY_PATH/i export PATH=\"${scheme}/bin:\$PATH\""
   '';
 
   # There's no test suite (?!).
@@ -36,26 +38,18 @@ stdenv.mkDerivation rec {
   setupHook = ./setup-hook.sh;
 
   meta = {
-    description = "The SLIB Portable Scheme Library";
-
+    description = "A portable Scheme utility library";
     longDescription = ''
       SLIB is a portable library for the programming language Scheme.  It
       provides a platform independent framework for using packages of Scheme
-      procedures and syntax.  As distributed, SLIB contains useful packages
-      for all Scheme implementations.  Its catalog can be transparently
-      extended to accommodate packages specific to a site, implementation,
-      user, or directory.
+      procedures and syntax.
 
       SLIB supports Bigloo, Chez, ELK 3.0, Gambit 4.0, Guile, JScheme, Kawa,
-      Larceny, MacScheme, MIT/GNU Scheme, Pocket Scheme, RScheme, scheme->C,
+      Larceny, MacScheme, MIT Scheme, Pocket Scheme, RScheme, scheme->C,
       Scheme48, SCM, SCM Mac, scsh, sisc, Stk, T3.1, umb-scheme, and VSCM.
     '';
-
-    # Public domain + permissive (non-copyleft) licensing of some files.
     license = stdenv.lib.licenses.publicDomain;
-
     homepage = http://people.csail.mit.edu/jaffer/SLIB;
-
-    maintainers = [ ];
+    maintainers = with stdenv.lib.maintainers; [ taktoa ];
   };
 }
