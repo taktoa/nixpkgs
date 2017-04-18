ad4a06ec5e8d280e5adfd4a33ba83d9be154496e
Remy Goldschmidt <taktoa@gmail.com>
2017-04-18 01:46:47 -0500

    mingw-w64: refactor and add enable-idl / enable-secure-api flags

--- pkgs/os-specific/windows/mingw-w64/default.nix
+++ pkgs/os-specific/windows/mingw-w64/default.nix
@@ -5,32 +5,48 @@
 
 let
   version = "5.0.2";
-  name = "mingw-w64-${version}";
-in
-stdenv.mkDerivation ({
-  inherit name;
 
-  src = fetchurl {
-    url = "mirror://sourceforge/mingw-w64/mingw-w64-v${version}.tar.bz2";
-    sha256 = "1z9cv77k4x7g95jk0ibwp8n2d66zx7d46ds5lcvjl459y47yhijz";
+  headersBuild = {
+    name = "mingw-w64-headers-${version}";
+
+    preConfigure = ''
+        cd mingw-w64-headers
+    '';
   };
-} //
-(if onlyHeaders then {
-  name = name + "-headers";
-  preConfigure = ''
-    cd mingw-w64-headers
-  '';
-  configureFlags = "--without-crt";
-} else if onlyPthreads then {
-  name = name + "-pthreads";
-  preConfigure = ''
-    cd mingw-w64-libraries/winpthreads
-  '';
-} else {
-  buildInputs = [ gccCross binutilsCross ];
 
-  crossConfig = gccCross.crossConfig;
+  pthreadsBuild = {
+    name = "mingw-w64-pthreads-${version}";
 
-  dontStrip = true;
-})
-)
+    preConfigure = ''
+        cd mingw-w64-libraries/winpthreads
+    '';
+  };
+
+  genericBuild = {
+    name = "mingw-w64-${version}";
+
+    buildInputs = [ gccCross binutilsCross ];
+
+    crossConfig = gccCross.crossConfig;
+
+    dontStrip = true;
+  };
+
+  common = {
+    src = fetchurl {
+      url = "mirror://sourceforge/mingw-w64/mingw-w64-v${version}.tar.bz2";
+      sha256 = "1z9cv77k4x7g95jk0ibwp8n2d66zx7d46ds5lcvjl459y47yhijz";
+    };
+
+    configureFlags = [
+      "--enable-idl"
+      "--enable-secure-api"
+    ];
+  };
+
+  specific = (
+    if      onlyHeaders  then headersBuild
+    else if onlyPthreads then pthreadsBuild
+    else                      genericBuild
+  );
+in stdenv.mkDerivation (common // specific)
